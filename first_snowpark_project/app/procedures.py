bca72da34181c99c41bd4dcc401956689702f569
inzamamshajahan <85426918+inzamamshajahan@users.noreply.github.com>
2024-10-12 14:13:21 +0000

    116

--- first_snowpark_project/app/procedures.py
+++ first_snowpark_project/app/procedures.py
@@ -7,8 +7,8 @@ from common import print_hello
 from snowflake.snowpark import Session
 
 
-def hello_procedure(session: Session, name: str) -> str:
-    return print_hello(name)
+# def hello_procedure(session: Session, name: str) -> str:
+#     return print_hello(name)
 
 
 def test_procedure(session: Session) -> str:
